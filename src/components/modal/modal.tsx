3bf4e5be6074de40bb645de88fcd9f7bf63bda87
Christophe Viau <christopheviau@gmail.com>
2019-04-25 17:44:03 -0400

    add minimal interface

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -2,27 +2,21 @@ import * as React from "react"
 import * as RMWC from "@rmwc/types"
 
 /**
- * Button properties.
+ * Modal properties.
  */
 export interface IModalProps {
-  /** Make the button unelevated. */
-  unelevated?: boolean
-  /** Make the button outlined. */
-  outlined?: boolean
-  /** Make the button disabled */
-  disabled?: boolean
-  /** Content specified as a label prop. */
-  label?: React.ReactNode | any
-  /** Content specified as children. */
-  children?: React.ReactNode
-  /** An Icon for the Button */
-  icon?: RMWC.IconPropT
-  /** A trailing icon for the Button */
-  trailingIcon?: RMWC.IconPropT
+  /** Whether or not the Dialog is showing. */
+  open?: boolean;
+  /** Callback for when the Dialog opens. */
+  onOpen?: (evt: RMWC.CustomEventT<{}>) => void;
+  /** Callback for when the Dialog closes. */
+  onClose?: (evt: RMWC.CustomEventT<{
+      action?: string;
+  }>) => void;
 }
 
 /**
- * button
+ * modal
  */
 export const Modal = (props: IModalProps) => {
   return <div>{props}</div>

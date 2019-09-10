09ef56838938503e397dda519586526d62960bf0
Christophe Viau <christopheviau@gmail.com>
2019-09-10 17:42:41 -0400

    first draft of dark mode

---
+++ src/theme-switcher.js
@@ -0,0 +1,17 @@
+const themeSwitcher =
+  "<div class='theme-switcher'>Theme: <button>Light</button></div>"
+const body = window.document.body
+body.childNodes[0].insertAdjacentHTML("beforebegin", themeSwitcher)
+
+const button = body.querySelector(".theme-switcher button")
+button.addEventListener(
+  "click",
+  () => {
+    body.classList.toggle("dark")
+    button.innerText = body.classList.contains("dark") ? "Dark" : "Light"
+  },
+  false
+)
+
+// hardcoded to dark
+body.classList.add("dark")

20f12b6a34ea8b4ff6516fdc4fe37eccccdd9a4d
Tamara Chu <tamara.chu@omnisci.com>
2020-01-06 13:59:25 -0800

    Build dist/ files

--- dist/components/dialog/Dialog.d.ts
+++ dist/components/dialog/Dialog.d.ts
@@ -13,9 +13,17 @@ export interface ISimpleDialogProps {
     message?: React.ReactNode | string;
     primaryLabel?: React.ReactNode | string;
     secondaryLabel?: React.ReactNode | string;
+    primaryAction?(): void;
+    secondaryAction?(): void;
     type?: string;
     hideCloseIcon?: boolean;
     onCloseFromHeader?: any;
+    children?: React.ReactNode;
+    footer?: React.ReactNode;
+    className?: any;
+    onStateChange?: any;
+    preventOutsideDismiss?: any;
+    actionToApplyOnEnter?: any;
 }
 export interface IDialogProps {
     /** Whether or not the Dialog is showing. */
@@ -24,7 +32,7 @@ export interface IDialogProps {
     onOpen?: any;
     /** Callback for when the Dialog closes. */
     onClose?: any;
-    children?: any;
+    children?: React.ReactNode;
 }
 /**
  * Dialog

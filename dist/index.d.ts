5e0c43c09f9b41a978006db86896437b126a74ab
Christie Villanueva <christie.villanueva@omnisci.com>
2020-04-14 16:13:16 -0700

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,6 +5,5 @@ import MultiSelect from "./components/multi-select/Multi-select";
 import Select from "./components/select/Select";
 import Toggler from "./components/toggler/Toggler";
 import List from "./components/list/List";
-
 import "./index.scss";
 export { Button, Dialog, IconButton, MultiSelect, Select, Toggler, List };

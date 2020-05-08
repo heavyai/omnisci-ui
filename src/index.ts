38150ca0b7b38c00d9f90f868d1d35ed84dbe722
Christophe Viau <christopheviau@gmail.com>
2020-05-08 14:55:50 -0400

    button height; list colors

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import Select from "./components/select/Select"
 import Toggler from "./components/toggler/Toggler"
 import List from "./components/list/List"
 
+import "./vars.scss"
 import "./index.scss"
 
 export { Button, Dialog, IconButton, MultiSelect, Select, Toggler, List }
